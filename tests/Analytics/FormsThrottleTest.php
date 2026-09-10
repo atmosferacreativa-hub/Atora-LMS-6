3e42bebfdb6041c571c2d697681d99cc4d1667c5
Atmósfera Creativa <atmosfera.creativa@gmail.com>
2026-09-10 15:10:16 -0400

    test(analytics): route proxy filter through deterministic store

--- tests/Analytics/FormsThrottleTest.php
+++ tests/Analytics/FormsThrottleTest.php
@@ -85,6 +85,14 @@ class FormsThrottleTest extends TestCase {
 
 	protected function setUp(): void {
 		parent::setUp();
+		\Brain\Monkey\Functions\when( 'apply_filters' )->alias(
+			static function( string $hook, $value, ...$args ) {
+				foreach ( $GLOBALS['__atora_test_filters'][ $hook ] ?? array() as $entry ) {
+					$value = call_user_func( $entry['cb'], $value, ...$args );
+				}
+				return $value;
+			}
+		);
 		atora_test_reset_post_meta();
 		atora_test_reset_filters();
 		$_SERVER['REMOTE_ADDR'] = '203.0.113.10';
